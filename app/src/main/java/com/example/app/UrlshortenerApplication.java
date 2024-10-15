5a7a7cbe99f76d2ea839281fe241bfe4068597c3
GabrielMelhem <gabrielmelhem@gmail.com>
2024-10-15 10:14:47 +0200

    customizing the solution for different Environments

--- app/src/main/java/com/example/app/UrlshortenerApplication.java
+++ app/src/main/java/com/example/app/UrlshortenerApplication.java
@@ -3,7 +3,6 @@ package com.example.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
@@ -14,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 @EntityScan(basePackages ="com.example.store.entity")
 @EnableJpaRepositories(basePackages ="com.example.store.repository")
-@PropertySource("classpath:env.properties")
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
